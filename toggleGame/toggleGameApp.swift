d7718a3a5bd81324f59867b792ab1050b859f1ad
AhmedAshrafghonemy <AhmedAshrafghonemy@gmail.com>
2021-12-07 01:29:17 +0200

    Initial Commit

---
+++ toggleGame/toggleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  toggleGameApp.swift
+//  toggleGame
+//
+//  Created by macbook on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct toggleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
