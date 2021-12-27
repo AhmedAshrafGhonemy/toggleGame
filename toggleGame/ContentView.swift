7e9e457eb42e26afc6b163acf7fd57fd40a02b52
AhmedAshrafghonemy <AhmedAshrafghonemy@gmail.com>
2021-12-27 20:29:38 +0200

    second edit

--- toggleGame/ContentView.swift
+++ toggleGame/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojies: Array<String> = ["🚠" , "🚁" , "⛴" , "🚘"]
+    
     var body: some View {
-        Text("Hello,earth")
-            .padding()
+       
+        HStack {
+            cardView(content: emojies[0])
+            cardView(content: emojies[1])
+            cardView(content: emojies[2])
+            cardView(content: emojies[3])
+        }
+        .foregroundColor(.red)
+        .padding(.horizontal)
+        
     }
 }
+
+struct cardView: View {
+    var content: String
+   @State var isFaceUp: Bool = false
+
+    var body: some View{
+                
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp{
+                shape.fill().foregroundColor(.white)
+               shape.stroke(lineWidth: 3)
+            Text(content)
+                .font(.largeTitle)
+        }
+        else{
+            shape.fill()
+        }
+    }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+.previewInterfaceOrientation(.portraitUpsideDown)
+        ContentView()
+            .preferredColorScheme(.light)
     }
+}
 }
